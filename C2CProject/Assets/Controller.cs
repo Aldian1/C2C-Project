fd2906ce724945d88ad34301bf9402cebc7d1c2b
Aldian1 <Aldian@hotmail.co.uk>
2016-03-23 23:40:55 +0000

    Added:
    -Anim:
    	-pause
    	-start
    	-increase speed
    	-decrease speed
    -Cart:
    	-Change Seat
    
    Todo:
    Need to add multiple cart support.
    Add occulus support.
    Follow up up with lead and see what else i need to do

---
+++ C2CProject/Assets/Controller.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour {
+
+	public Text CurrentFrame, Speed, Cart;
+
+	public Animator main_animator;
+
+	public GameObject cart;
+
+	public string playbackspeed;
+
+	public string currentcar;
+
+	public int switcher;
+
+
+	public float playbacktime;
+
+	public AnimatorClipInfo[] animationclip;
+
+	public GameObject[] Cart1Seats;
+
+	private int currentseat;
+
+	// Use this for initialization
+	void Start () {
+		main_animator = cart.GetComponent<Animator> ();
+		playbackspeed = "x1";
+	
+	
+
+		//animationclip = main_animator.GetCurrentAnimatorClipInfo (0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		currentcar = transform.parent.name;
+		Cart.text = currentcar;
+		Speed.text = playbackspeed;
+
+		switch (switcher) {
+
+		case 0: 
+			playbackspeed = "x1"; 
+			main_animator.speed = 1;
+
+		break;
+		case 1: 
+			playbackspeed = "x2";
+			main_animator.speed = 2;
+			break;
+
+		case 2:
+			playbackspeed = "x3";
+			main_animator.speed = 3;
+			break;
+
+		case 3:
+			playbackspeed = "Paused";
+			main_animator.speed = 0;
+
+			break;
+		}
+	}
+
+	public void PauseAnimation()
+	{
+		switcher = 3;
+	}
+
+	public void StartAnimation()
+	{
+		switcher = 0;
+
+	}
+
+	public void IncreaseSpeed()
+	{
+		if (switcher < 2) {
+
+			switcher += 1;
+
+		}
+	}
+
+	public void DecreaseSpeed()
+	{
+		if (switcher > 0) {
+			switcher -= 1;
+		}
+	}
+
+	public void ChangeSeat()
+	{
+		if (currentseat >= Cart1Seats.Length - 1) {
+			currentseat = 0;
+			transform.position = Cart1Seats [currentseat].transform.position;
+			return;
+		}
+
+		currentseat += 1;
+		transform.position = Cart1Seats [currentseat].transform.position;
+	}
+}
